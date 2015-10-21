60f47c213c2f1afd0a7041f1bf49e433db59f95d
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-21 10:15:39 -0400

    added color flash on alarm logic

--- ctlClockLib/ctlAlarmClock.cs
+++ ctlClockLib/ctlAlarmClock.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Filename: ctlAlarmClock.cs
+// Author: Brahm Ramkissoon
+// Created Date  (dd/mm/yyyy): 21/10/2015
+// Description: inherit from ctlClock
+// Revisions    - add color flash functionality on alarm
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +18,7 @@ namespace ctlClockLib
         // Private Instance Variables
         private DateTime _dteAlarmTime;
         private bool _blnAlarmSet;
+        private bool _blnColorTicker;       // flash alarm if true
 
         // Public Properties
         // These properties will be declared as public to alllow future devs to accesss them
@@ -43,6 +49,47 @@ namespace ctlClockLib
         public ctlAlarmClock ()
         {
             InitializeComponent();
+        }
+
+        protected override void timer1_Tick ( object sender, EventArgs e )
+        {
+            base.timer1_Tick(sender, e);
+
+            // Checks to see if the alarm is set
+            if (AlarmSet == false)
+            {
+                return;
+            }
+            else 
+            // If the date, hour and minute of the alarm time are the same as
+            // the current ime, flash an alarm
+            {
+                if (AlarmTime.Date == DateTime.Now.Date &&
+                    AlarmTime.Hour == DateTime.Now.Hour &&
+                    AlarmTime.Minute == DateTime.Now.Minute)
+                {
+                    // Sets lblAlarmVisble to true, and changes the background color based on
+                    // The value of blnColorTicker. The backgrond color of the lable
+                    // will flash once per tick o the clock.
+
+                    lblAlarm.Visible = true;
+                    if (_blnColorTicker == false)
+                    {
+                        lblAlarm.BackColor = Color.Red;
+                        _blnColorTicker = true;
+                    }
+                    else
+                    {
+                        lblAlarm.BackColor = Color.Blue;
+                        _blnColorTicker = false;
+                    }
+                }
+                else
+                {
+                    //Once the alarm has sounded for a minute, the label is made invisble again
+                    lblAlarm.Visible = false;
+                }
+            }
         }
     }
 }
