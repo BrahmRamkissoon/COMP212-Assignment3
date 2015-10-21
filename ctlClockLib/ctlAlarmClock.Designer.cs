d61d32f2cb2ab957ad141929c6b4b5adafd1f4f7
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-21 10:03:35 -0400

    Added lblAlarm

--- ctlClockLib/ctlAlarmClock.Designer.cs
+++ ctlClockLib/ctlAlarmClock.Designer.cs
@@ -28,9 +28,33 @@
         /// </summary>
         private void InitializeComponent ()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAlarm
+            // 
+            this.lblAlarm.AutoSize = true;
+            this.lblAlarm.Location = new System.Drawing.Point(4, 30);
+            this.lblAlarm.Name = "lblAlarm";
+            this.lblAlarm.Size = new System.Drawing.Size(36, 13);
+            this.lblAlarm.TabIndex = 1;
+            this.lblAlarm.Text = "Alarm!";
+            this.lblAlarm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAlarm.Visible = false;
+            // 
+            // ctlAlarmClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Controls.Add(this.lblAlarm);
+            this.Name = "ctlAlarmClock";
+            this.Controls.SetChildIndex(this.lblAlarm, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAlarm;
     }
 }
