d75b0b5ef7fca793571266b475981103424cdc8a
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-21 10:01:27 -0400

    added properties AlarmTime and AlarmSet

--- ctlClockLib/ctlAlarmClock.cs
+++ ctlClockLib/ctlAlarmClock.cs
@@ -10,6 +10,36 @@ namespace ctlClockLib
 {
     public partial class ctlAlarmClock : ctlClockLib.ctlClock
     {
+        // Private Instance Variables
+        private DateTime _dteAlarmTime;
+        private bool _blnAlarmSet;
+
+        // Public Properties
+        // These properties will be declared as public to alllow future devs to accesss them
+
+        // store value of date and time alarm is set to
+        public DateTime AlarmTime
+        {
+            get { return _dteAlarmTime; }
+            set
+            {
+                _dteAlarmTime = value;
+            }
+        }
+
+        // indicate whether the alarm is set
+        public bool AlarmSet
+        {
+            get
+            {
+                return _blnAlarmSet;
+            }
+            set
+            {
+                _blnAlarmSet = value;
+            }
+        }
+
         public ctlAlarmClock ()
         {
             InitializeComponent();
