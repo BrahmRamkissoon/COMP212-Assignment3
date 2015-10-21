ff67bdb3f4cce8d12d24295f02a0810902bfef01
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-21 09:48:13 -0400

    Added custom property

--- ctlClockLib/ctlClock.cs
+++ ctlClockLib/ctlClock.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// Filename: ctlClock.cs
+// Author: Brahm Ramkissoon
+// Created Date  (dd/mm/yyyy): 21/10/2015
+// Description:  Create a composite control:
+/* Revisions    - created composite control of timer and lablel
+**              - added timer event
+**              - added a custom property 
+**
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -12,6 +21,27 @@ namespace ctlClockLib
 {
     public partial class ctlClock: UserControl
     {
+        // Private Variables
+        private Color colBColor;
+
+        // Public properties
+        // Declares name and type of prop
+        public Color ClockBackColor
+        {
+            // Retrieves the value of the private variable colBColor
+            get
+            {
+                return colBColor;
+            }
+
+            // Stores the selected value in the private variable colBColor, and
+            // Updates the background color of the label control lblDisplay.
+            set
+            {
+                colBColor = value;
+                lblDisplay.BackColor = colBColor;
+            }
+        }
         public ctlClock()
         {
             InitializeComponent();
